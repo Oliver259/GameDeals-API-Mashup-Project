7f435cb5e97c9f70edcb4386d87af5a2fab8cadc
Oliver Gray <115050339+Oliver259@users.noreply.github.com>
2023-08-31 13:15:58 +1000

    Modified index.js to show all wishlisted games

--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,27 @@ var express = require("express");
 var router = express.Router();
 
 // API_KEY = "EA1D9E34754E899A4983EC1A9F1C687E";
-const STEAM_ID = "76561198277446098";
 /* GET home page. */
-
-// router.get("/", function (req, res, next) {
-//   fetch(
-//     `https://store.steampowered.com/wishlist/profiles/${STEAM_ID}/wishlistdata/`
-//   )
-//     .then((wishlistResponse) => wishlistResponse.json())
-//     .then((wishlistData) => {
-//       const wishlistedGames = Object.values(wishlistData);
-//       res.render("index", {
-//         title: "Wishlist",
-//         games: wishlistedGames,
-//       });
-//     })
-
-//     .catch((error) => {
-//       console.error("Error fetching wishlist data:", error);
-//       res.render("error", {
-//         title: "Error",
-//         games: [],
-//         error: error, // Pass the error object to the view
-//       });
-//     });
-
-// });
 
 // TODO: Add UI for game search and game deals so the user can see the response
 
 router.get("/", async function (req, res, next) {
   try {
-    const steamWishlistResponse = await fetch(
-      `https://store.steampowered.com/wishlist/profiles/${STEAM_ID}/wishlistdata/`
-    );
+    const STEAM_ID = "76561198277446098";
+    const totalPages = 10;
+    const wishlistedGames = [];
+
+    for (let page = 0; page < totalPages; page++) {
+      const steamWishlistResponse = await fetch(
+        `https://store.steampowered.com/wishlist/profiles/${STEAM_ID}/wishlistdata/?p=${page}`
+        );
+    
+
     const steamWishlistData = await steamWishlistResponse.json();
 
-    const wishlistedGames = Object.values(steamWishlistData);
+    const gamesOnPage = Object.values(steamWishlistData);
+    wishlistedGames.push(...gamesOnPage);
+  }
 
     res.render("index", {
       title: "Wishlist",
